4c86a6e757e6271c490699a5fb14e92e667c4cda
Joel Montana <montana12@outlook.es>
2022-02-01 12:22:52 -0600

    Agregando cambios

---
+++ Ejercicio2/cadenastring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string nombre;
+    cout<<"Ingrese su nombre: ";
+    cin>>nombre;
+    cout<<endl;
+
+    cout<<"Su nombre es: "<<nombre;
+    cout<<endl;
+    return 0;
+}
