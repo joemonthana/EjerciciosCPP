b910421bcd6a504cbce656d6dc34926b1c7a37d2
Joel Montana <montana12@outlook.es>
2022-02-01 15:04:21 -0600

    Agregando ejercicio variables

---
+++ Ejercicio - variables/Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int NumeroEntero;
+    NumeroEntero = 25;
+    cout << "El numero entero es: " << NumeroEntero << endl;
+
+    NumeroEntero = 7;
+
+    cout <<"En numero entero modificado es: " << NumeroEntero << endl;
+    return 0;
+}
