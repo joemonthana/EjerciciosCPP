f62e3ba53fa2468cad28b34ec8de47465655761e
Joel Montana <montana12@outlook.es>
2022-02-01 15:26:39 -0600

    Agregando Ejercicio todas las variables

---
+++ Ejercicio - Todas las variables/Todaslasvariables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    bool valorBooleano = true;
+    int valorentero = 20;
+    double valordecimal = 3.1416;
+    char Valorcaracter = 'a';
+    string valorcadena = "Hola Mundo";
+
+    cout << "Valor boleano: " <<valorBooleano <<endl;
+    cout << "Valor entero: " <<valorentero <<endl;
+    cout << "Valor decimal: " <<valordecimal <<endl;
+    cout << "Valor Caracter corto: " <<Valorcaracter <<endl;
+    cout << "Valor cadena: " <<valorcadena <<endl;
+    return 0;
+}
