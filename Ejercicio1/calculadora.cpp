eeedece0938452fed17807ebe8cf2647c9f516aa
Joel Montana <montana12@outlook.es>
2022-02-01 13:08:48 -0600

    Agregando calculadora

---
+++ Ejercicio1/calculadora.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int b = 0;
+
+    cout<<"Ingrese el valor de a: ";
+    cin>>a;
+    cout<<endl;
+    cout<<"Ingrese el valor de b: ";
+    cin>>b;
+    cout<<endl;
+
+    cout<<"La suma de a y b es: " <<a+b;
+    cout<<endl;
+    cout<<"La resta de a y b es: " <<a-b;
+    cout<<endl;
+    cout<<"La division de a y b es: " <<a/b;
+    cout<<endl;
+    cout<<"La multiplicacion de a y b es : " <<a*b;
+    cout<<endl;
+
+    return 0;
+}
