38a7f45c45f044c47e8ece19005b1e1b47a07f5b
Joel Montana <montana12@outlook.es>
2022-02-16 18:04:20 -0600

    modificando

---
+++ Ejercicio16 - Ciclos While/CiclosWhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a = 5;
+    int b = 2;
+    
+    while (a > b) 
+    {
+        cout <<"Ingrese el valor de a: ";
+        cin >> a;
+    }
+    cout <<"Saliste del ciclo";
+    return 0;
+}
