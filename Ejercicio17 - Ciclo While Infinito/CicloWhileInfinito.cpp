7745221a3b07d823f09bbf290c35a16381a90b23
Joel Montana <montana12@outlook.es>
2022-02-16 18:31:08 -0600

    ClicloWhileInfinito

---
+++ Ejercicio17 - Ciclo While Infinito/CicloWhileInfinito.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int ciclo = 0;
+    while (ciclo < 100)
+    {
+        cout << "*";
+        ciclo = ciclo + 1;
+    }
+    
+    return 0;
+}
