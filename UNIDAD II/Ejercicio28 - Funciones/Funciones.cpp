6b50a8a546718a3ec3893883ae2e94e9d06a45ee
Joel Montana <montana12@outlook.es>
2022-03-12 19:00:56 -0600

    modificando

--- UNIDAD II/Ejercicio28 - Funciones/Funciones.cpp
+++ UNIDAD II/Ejercicio28 - Funciones/Funciones.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int sumar() {
+    return 5 + 7;
+}
+
+string nombreCompleto() {
+    string nombre = "Juan";
+    string apellido = "Perez";
+    return nombre + " " + apellido;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int resultado = 0;
+    string nombreyApellido;
+    resultado = sumar();
+    nombreyApellido = nombreCompleto();
+    cout << resultado;
+    cout << endl;
+    cout << nombreyApellido;
     return 0;
 }
